4feed83fa679e23009800c76dc3c76262b91b468
rajvjeevan <rajiv1986819@gmail.com>
2021-12-21 08:45:48 +0530

    hello world ah

---
+++ destroyOrSave/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  destroyOrSave
+//
+//  Created by Rajiv on 2021-12-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Hello world")
+    }
+
+
+}
+
